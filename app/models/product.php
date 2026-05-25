123c3ccae2979efdd77c828dfd322dcdca2fc45b
rike0622 <112202407281@mhs.dinus.ac.id>
2026-05-25 19:45:15 +0700

    2

---
+++ app/models/product.php
@@ -0,0 +1,76 @@
+<?php
+class Product {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getAll() {
+        $result = $this->conn->query("SELECT * FROM products ORDER BY tanggal_masuk DESC");
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function getById($id) {
+        $stmt = $this->conn->prepare("SELECT * FROM products WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    public function create($data) {
+        $stmt = $this->conn->prepare("
+            INSERT INTO products (nama_produk, harga, stok, gambar) 
+            VALUES (?, ?, ?, ?)
+        ");
+        $stmt->bind_param("sdis", 
+            $data['nama_produk'], 
+            $data['harga'], 
+            $data['stok'], 
+            $data['gambar']
+        );
+        return $stmt->execute();
+    }
+
+    public function update($id, $data) {
+        
+        if (!empty($data['gambar'])) {
+            $stmt = $this->conn->prepare("
+                UPDATE products SET nama_produk=?, harga=?, stok=?, gambar=? 
+                WHERE id=?
+            ");
+            $stmt->bind_param("sdisi", 
+                $data['nama_produk'], 
+                $data['harga'], 
+                $data['stok'], 
+                $data['gambar'], 
+                $id
+            );
+        } else {
+            
+            $stmt = $this->conn->prepare("
+                UPDATE products SET nama_produk=?, harga=?, stok=? 
+                WHERE id=?
+            ");
+            $stmt->bind_param("sdsi", 
+                $data['nama_produk'], 
+                $data['harga'], 
+                $data['stok'], 
+                $id
+            );
+        }
+        return $stmt->execute();
+    }
+
+    public function delete($id) {
+        $stmt = $this->conn->prepare("DELETE FROM products WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
+    }
+}
+?>
