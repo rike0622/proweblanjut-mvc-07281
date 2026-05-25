b42c15a409593cdb706d2dc2c504fc2d750a8c02
rike0622 <112202407281@mhs.dinus.ac.id>
2026-05-25 20:17:29 +0700

    10

--- app/models/product.php
+++ app/models/product.php
@@ -9,9 +9,7 @@ class Product {
     public function getAll() {
         $result = $this->conn->query("SELECT * FROM products ORDER BY tanggal_masuk DESC");
         $data = [];
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-        }
+        while ($row = $result->fetch_assoc()) { $data[] = $row; }
         return $data;
     }
 
@@ -19,51 +17,19 @@ class Product {
         $stmt = $this->conn->prepare("SELECT * FROM products WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
+        return $stmt->get_result()->fetch_assoc();
     }
 
     public function create($data) {
-        $stmt = $this->conn->prepare("
-            INSERT INTO products (nama_produk, harga, stok, gambar) 
-            VALUES (?, ?, ?, ?)
-        ");
-        $stmt->bind_param("sdis", 
-            $data['nama_produk'], 
-            $data['harga'], 
-            $data['stok'], 
-            $data['gambar']
-        );
+        $stmt = $this->conn->prepare("INSERT INTO products (nama_produk, harga, stok, gambar) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("sdis", $data['nama_produk'], $data['harga'], $data['stok'], $data['gambar']);
         return $stmt->execute();
     }
 
     public function update($id, $data) {
-        
-        if (!empty($data['gambar'])) {
-            $stmt = $this->conn->prepare("
-                UPDATE products SET nama_produk=?, harga=?, stok=?, gambar=? 
-                WHERE id=?
-            ");
-            $stmt->bind_param("sdisi", 
-                $data['nama_produk'], 
-                $data['harga'], 
-                $data['stok'], 
-                $data['gambar'], 
-                $id
-            );
-        } else {
-            
-            $stmt = $this->conn->prepare("
-                UPDATE products SET nama_produk=?, harga=?, stok=? 
-                WHERE id=?
-            ");
-            $stmt->bind_param("sdsi", 
-                $data['nama_produk'], 
-                $data['harga'], 
-                $data['stok'], 
-                $id
-            );
-        }
+        // Selalu update gambar agar sinkron dengan controller
+        $stmt = $this->conn->prepare("UPDATE products SET nama_produk=?, harga=?, stok=?, gambar=? WHERE id=?");
+        $stmt->bind_param("sdisi", $data['nama_produk'], $data['harga'], $data['stok'], $data['gambar'], $id);
         return $stmt->execute();
     }
 
